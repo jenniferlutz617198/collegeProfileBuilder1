2063d9ec2acaa12f757c22c4458632f986374ad7
student3 <student3@jh-mbair-158-14.d214.org>
2016-03-01 14:47:44 -0600

    sixth commit

--- collegeProfileBuilder1/DetailsViewController.swift
+++ collegeProfileBuilder1/DetailsViewController.swift
@@ -12,20 +12,24 @@ class DetailsViewController: UIViewController {
     var currentCollege = College()
 
     @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var numberOfStudentsTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = currentCollege.name
-        myLabel.text = currentCollege.location
         nameTextField.text = ""
         numberOfStudentsTextField.text = ""
+        locationTextField.text = ""
+        
         
         
     }
     
-    var image : UIImage
+    var imageView = UIImage()
+    
+    
 
+    
     
 }
